5b127efe4d0472061a6a0bf7f7b3db52a0b985c1
LyDD <lydd42@gmail.com>
2016-07-30 23:42:01 +0100

    follow google c++ style to name file name with lower case letters

---
+++ QLab/utilities/constrained_value.hpp
@@ -0,0 +1,96 @@
+
+#include <stdexcept>
+
+namespace QLab
+{
+
+
+template <class ValuePolicy>
+class ConstrainedValue
+{
+public:
+    using ValueT = typename ValuePolicy::ValueT;
+    
+    // TODO: check if assign value_ some value
+    ConstrainedValue (ValueT value) {
+        if (ValuePolicy::validate(value)) {
+            value_  = value;
+        } else {
+            ValuePolicy::on_error(value);
+        }
+        
+    }
+    // all cases expecting ValueT should accept ConstrainedValueT<ValuePolicy<ValueT>>
+    operator ValueT () const { return value_; }
+
+private:
+    ValueT value_;
+};
+
+
+template <typename ValueT_, ValueT_ min>
+class LowerBounded
+{
+public:
+    using ValueT = ValueT_;
+
+    static bool validate (ValueT value) {
+        if (value < min) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static void on_error(ValueT value) {
+        throw std::out_of_range("violated lower bound constraint");
+    }
+
+};
+
+
+template <typename ValueT_, ValueT_ max>
+class UpperBounded
+{
+public:
+    using ValueT = ValueT_;
+
+    static bool validate (ValueT value) {
+        if (value < max) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static void on_error(ValueT value) {
+        throw std::out_of_range("violated upper bound constraint");
+    }
+
+};
+
+
+template <class ValueT_, ValueT_ min, ValueT_ max>
+class BiBounded {
+public:
+    using ValueT = ValueT_;
+
+    static bool validate (ValueT value) {
+        if (value < min) {
+            return false;
+        } else if (value > max) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+    
+    static void on_error(ValueT value) {
+        throw std::out_of_range("violated bi bound constraint");
+    }
+
+};
+
+
+} // namespace QLab  
