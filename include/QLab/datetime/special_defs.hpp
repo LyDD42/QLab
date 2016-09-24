58495b2feeaf0defa4c0ff5915cb68a754c368d3
LyDD <lydd42@gmail.com>
2016-09-24 23:01:38 +0100

    { on previous line

---
+++ include/QLab/datetime/special_defs.hpp
@@ -0,0 +1,19 @@
+#ifndef SPECIAL_DEFS_HPP_OFHRIQCA
+#define SPECIAL_DEFS_HPP_OFHRIQCA
+
+
+namespace  QLab {
+namespace datetime {
+
+
+enum class special_values {
+    Minimum = -1,
+    Maximum = 1,
+    NaT     = 0,
+};
+
+
+} // namespace datetime  
+} // namespace  QLab  
+
+#endif // end of include guard: SPECIAL_DEFS_HPP_OFHRIQCA
